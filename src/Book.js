525c7f507886fe765af29e8f78aa45ea095b572f
James Leja <James_Leja@otpp.com>
2019-07-15 14:40:12 -0400

    Get CSS to work finally...I hate it

--- src/Book.js
+++ src/Book.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import './Book.css'
 
 export const Book = ({title, author, pages, favouriteBook, currentlyReading, image}) => {
 	return (
-		<div className="book" display="flex">
-		    <img src={image} width="200" height="300" float="right" alt="Book cover"/>
-			<div text-align="right" display="inline-block" float="right">{title} by: {author} {pages} pages long</div>
-			<div>Favourite Book: {favouriteBook ? 'yes!': 'no!'}</div>
-			<div>You {currentlyReading ? 'are': 'are not'} currently reading the book</div>
+		<div className="book">
+      <img src={image} width="200" height="300" alt="Book cover"/>
+      <div className="bookInfo">
+        <p>{title} by: {author} {pages} pages long</p>
+        <p>Favourite Book: {favouriteBook ? 'yes!': 'no!'}</p>
+        <p>You {currentlyReading ? 'are': 'are not'} currently reading the book</p>
+      </div>
 		</div>
 	)
 }
